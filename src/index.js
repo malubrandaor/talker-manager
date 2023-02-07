54e17b5cfa1aadda5f9b6b30a49854e592163c22
Maria Luísa Brandão <malubrandaor@gmail.com>
2023-02-06 22:17:15 -0300

    feat: requisito 1 -- get com o path /talker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -6,9 +8,26 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const readTalkers = async () => {
+  const talkers = await fs.readFile(path.resolve(__dirname, 'talker.json'));
+  const talkersJson = JSON.parse(talkers);
+  // console.log(talkersJson);
+  return talkersJson;
+};
+
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  try {
+    const talker = await readTalkers();
+  res.status(200).json(talker);
+  } catch ({ message }) {
+    res.status(200).json([]);
+  }
 });
 
 app.listen(PORT, () => {
