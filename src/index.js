4f923e211674581f79bc67aaa3f97ebe85250579
Maria Luísa Brandão <malubrandaor@gmail.com>
2023-02-07 17:48:07 -0300

    feat: requisito 2 -- get com o path /talker:id

--- src/index.js
+++ src/index.js
@@ -8,13 +8,21 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// funçao que le o arquivo talker.json e converte para a linguagem js
+// requisito 1 = funçao que le o arquivo talker.json e converte para a linguagem js e retorna para o path definido
 const readTalkers = async () => {
   const talkers = await fs.readFile(path.resolve(__dirname, 'talker.json'));
   const talkersJson = JSON.parse(talkers);
   // console.log(talkersJson);
   return talkersJson;
 };
+
+// requisito 2 = funçao que busca o path pelo ultimo id 
+// const getTalkersById = async (req) => {
+//   const { id } = req.params;
+//   const talkersId = await readTalkers();
+//   const talkersFindId = talkersId.find((i) => i.id === Number(id));
+//   return talkersFindId;
+// };
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -25,10 +33,19 @@ app.get('/', (_request, response) => {
 app.get('/talker', async (req, res) => {
   try {
     const talker = await readTalkers();
-  res.status(200).json(talker);
+    res.status(200).json(talker);
   } catch ({ message }) {
     res.status(200).json([]);
   }
+});
+
+app.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const talkersId = await readTalkers();
+    const talkersFindId = talkersId.find((i) => i.id === Number(id));
+   if (!talkersFindId) 
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada'});
+   res.status(200).json(talkersFindId);
 });
 
 app.listen(PORT, () => {
